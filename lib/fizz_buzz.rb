80007676063737c8ce54cef25affdeaba72e3ccd
mhirosawa <hirosawa0338@gmail.com>
2018-03-25 12:00:39 +0900

    3章 FizzBuzzTest

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,20 +1,11 @@
 def fizz_buzz(num)
   if num % 15 == 0
-    puts 'Fizz Buzz'
+    'Fizz Buzz'
   elsif num % 3 == 0
-    puts 'Fizz'
+    'Fizz'
   elsif num % 5 == 0
-    puts 'Buzz'
+    'Buzz'
   else
-    puts num.to_s
+    num.to_s
   end
 end
-
-
-fizz_buzz(1)
-fizz_buzz(2)
-fizz_buzz(3)
-fizz_buzz(4)
-fizz_buzz(5)
-fizz_buzz(6)
-fizz_buzz(15)
